0fde7f5a5892fbf777f9d81285ad7eabb819e5e2
aleksandr_lantsev <alancev@yandex-team.ru>
2018-06-14 23:06:07 +0300

    styl support

--- src/syntaxes.js
+++ src/syntaxes.js
@@ -2,5 +2,6 @@ module.exports = {
   css: require('./css'),
   less: require('./less'),
   sass: require('./sass'),
-  scss: require('./scss')
+  scss: require('./scss'),
+  styl: require('./scss'),
 };
