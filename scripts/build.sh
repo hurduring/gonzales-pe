39bf24f55abdfa44a3a12a772313da0cb68a231b
aleksandr_lantsev <alancev@yandex-team.ru>
2018-06-14 23:10:02 +0300

    testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,14 +14,6 @@ printf "\n\
  Generating webpack modules ($syntaxes)\n\
 -------------------------------------------------\n\n"
 
-echo 'module.exports = {' > src/syntaxes.js
-
-for syntax in $syntaxes; do
-  echo "$syntax: require('./$syntax')," >> src/syntaxes.js
-done
-
-echo '};' >> src/syntaxes.js
-
 ./node_modules/.bin/webpack --module-bind "json=json"
 
 git checkout -- src/syntaxes.js
